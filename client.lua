6bdcce87e7ea18c29fbd441764db6d19214a3b4c
Gold14567 <70439470+Gold14567@users.noreply.github.com>
2022-09-26 11:46:30 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,20 @@
+Citizen.CreateThread(function()
+      while true do
+	     Citizen.Wait(0)
+		 
+		 -- Traffic and Ped density you can configure these
+		 SetTrafficDensity(0.3) -- 0.0 none, 1.0 max
+		 SetPedDensity(0.2) -- 0.0 none, 1.0 max
+	end	 
+end)
+
+Function SetTrafficDensity(density)
+       SetParkedVehicleDensityMultiplierThisFrame(density)
+	   SetVehicleDensityMultiplierThisFrame(density)
+	   SetRandomVehicleDensityMultiplierThisFrame(density)
+end
+
+Function SetPedDensity(density)
+       SetPedDensityMultiplierThisFrame(density)
+	   SetScenarioPedDensityThisFrame(density)
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TPC2018TR'
+description 'Simple NPC despawner'
+version '1.0.0'
+
+client_script 'client.lua'
